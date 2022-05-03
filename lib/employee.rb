d31354cc0542968c4e6c3fde5808c9dcbc485f6c
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-05-03 04:08:59 +0000

    add validation:

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,4 +1,12 @@
 class Employee < ActiveRecord::Base
-
   belongs_to :store
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, :inclusion => 40..200
+  validates_associated :store
+
+  def must_always_have_a_first_name
+
+
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,7 @@
 class Store < ActiveRecord::Base
-
   has_many :employees
+
+  validates :name, presence: true
+  validates :annual_revenue, comparison: { greater_than: 0}
 
 end
